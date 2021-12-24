b00ab6c467d968707ddd1a0aefb0324b0e67a518
106539 <106539@WLL1876P>
2021-12-24 18:16:27 +0530

    Add project files.

---
+++ ChildrenBookStore/Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ChildrenBookStore.Models;
+
+namespace ChildrenBookStore.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<ChildrenBookStore.Models.usersaccounts> usersaccounts { get; set; }
+        public DbSet<ChildrenBookStore.Models.orders> orders { get; set; }
+        public DbSet<ChildrenBookStore.Models.report> report { get; set; }
+
+    }
+}
